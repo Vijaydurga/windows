a6d79c4c9a992abc2d82f3e9b98eade891c59b88
vijaydurga <vijaydurga3@gmail.com>
2019-02-26 15:15:06 -0800

    windows

---
+++ windows_1/windows_1/Source.cpp
@@ -0,0 +1,59 @@
+/*#include<Windows.h>
+#include<stdio.h>
+#include<iostream>
+#include<tchar.h>
+using namespace std;
+
+int main()
+	{
+	char ansi[] = "ansi-string";
+	wchar_t widechar[] = L"widecharacter-string";
+	bool ret;
+	if (IsTextUnicode(ansi, strlen(ansi), NULL))
+		cout << "given is a unicode text" << endl;
+	else
+		cout << "not a unicode text ";
+
+	system("pause");
+	}*/
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	CHAR ansiarray[] = "ANSI";
+	WCHAR widearray[] = L"WIDE";
+	int ret, size;
+	ret = IsTextUnicode(ansiarray, strlen(ansiarray), NULL);
+	if (ret)
+		printf("it is a wide char \n");
+	else
+		printf("it is not a wide char \n");
+	ret = IsTextUnicode(widearray, wcslen(widearray), NULL);
+	if (ret)
+		printf("it is a wide char \n");
+	else
+		printf("it is not a wide char \n");
+	printf("the error no is:%d \n", GetLastError());
+	size = MultiByteToWideChar(CP_UTF8, 0, ansiarray, -1, NULL, 0);
+	WCHAR *arr1 = new WCHAR[size];
+	printf("the error no is:%d \n", GetLastError());
+	MultiByteToWideChar(CP_UTF8, 0, ansiarray, -1, arr1, size);
+	ret = IsTextUnicode(arr1, wcslen(arr1), NULL);
+	if (ret)
+		printf("it is a wide char \n");
+	else
+		printf("it is not a wide char \n");
+	printf("the error no is:%d \n", GetLastError());
+	size = WideCharToMultiByte(CP_UTF8, 0, widearray, -1, NULL, 0, NULL, NULL);
+	CHAR *arr2 = new CHAR[size];
+	WideCharToMultiByte(CP_UTF8, 0, widearray, -1, arr2, size,NULL,NULL);
+	ret = IsTextUnicode(arr1, wcslen(arr1), NULL);
+	if (ret)
+		printf("it is a wide char \n");
+	else
+		printf("it is not a wide char \n");
+	printf("the error no is:%d \n", GetLastError());
+
+	getchar();
+	return 0;
+}
